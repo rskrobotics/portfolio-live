513f905ef0388e554c9eb52ae42e556c6f624d5a
Krzysztof Kasprzyk <krzychu1232@gmail.com>
2021-07-10 13:19:26 +0200

    Fixed Urlpatterns to include media files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,7 +31,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-ALLOWED_HOSTS = ['crmo-portfolio.herokuapp.com', '127.0.0.1:8000',
+ALLOWED_HOSTS = ['crmo-portfolio.herokuapp.com', '127.0.0.1',
                  'rsk-code.pl']
 
 # Application definition
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -24,9 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += staticfiles_urlpatterns()
-
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
